45efff81c3ec60d3daf31128e26e248dfd0c5931
kazuyasuzuki-t <kazuya.suzuki@toda.co.jp>
2023-07-13 10:54:59 +0900

    add mui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Home: React.FC = () => {
+export default function Home() {
     return (
         <div>
             <h1>Hello World</h1>
         </div>
     );
-};
+}
